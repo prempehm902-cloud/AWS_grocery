33e67754f392fb927d7e81287160b8e8657df6c3
Michael Owusu Prempeh <prempehm902@gmail.com>
2026-02-15 23:41:12 +0100

    Update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -14,7 +14,6 @@ provider "aws" {
 ##############################
 # VPC
 ##############################
-
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
   tags       = { Name = "main-vpc" }
@@ -25,16 +24,26 @@ resource "aws_internet_gateway" "main" {
   tags   = { Name = "main-igw" }
 }
 
-##############################
-# Public Subnet
-##############################
-
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
   availability_zone       = var.public_az
   tags                    = { Name = "public-subnet" }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_1_cidr
+  availability_zone = var.private_az_1
+  tags              = { Name = "private-subnet-1" }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_2_cidr
+  availability_zone = var.private_az_2
+  tags              = { Name = "private-subnet-2" }
 }
 
 resource "aws_route_table" "public" {
@@ -54,39 +63,11 @@ resource "aws_route_table_association" "public_assoc" {
 }
 
 ##############################
-# Private Subnets
+# Security Groups
 ##############################
-
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_1_cidr
-  availability_zone = var.private_az_1
-  tags              = { Name = "private-subnet-1" }
-}
-
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_2_cidr
-  availability_zone = var.private_az_2
-  tags              = { Name = "private-subnet-2" }
-}
-
-##############################
-# RDS Subnet Group
-##############################
-
-resource "aws_db_subnet_group" "rds_subnet_group" {
-  name       = var.db_subnet_group_name
-  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-}
-
-##############################
-# Security Group for EC2
-##############################
-
 resource "aws_security_group" "app1_sg" {
   name        = "app1-sg"
-  description = "Allow SSH, HTTP, and Flask app traffic"
+  description = "Allow SSH, HTTP, Flask app traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -104,4 +85,50 @@ resource "aws_security_group" "app1_sg" {
   }
 
   ingress {
-    from_port_
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+##############################
+# IAM Role & Profile
+##############################
+resource "aws_iam_role" "grocery_ec2_role" {
+  name = "grocery_ec2_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Sid       = ""
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attach" {
+  role       = aws_iam_role.grocery_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_instance_profile" "joy_ec2_profile" {
+  name = "grocery_ec2_role"
+  role = aws_iam_role.grocery_ec2_role.name
+}
+
+##############################
+# RDS Subnet Group
+##############################
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = var.db_subnet_group_name
+  subnet_ids = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+}
