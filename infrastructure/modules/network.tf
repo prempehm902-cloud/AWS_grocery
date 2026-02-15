f9e2a6b9e5aaf0300cebc50345dd99063876db81
Michael Owusu Prempeh <prempehm902@gmail.com>
2026-02-15 12:10:16 +0100

    Update network.tf

--- infrastructure/modules/network.tf
+++ infrastructure/modules/network.tf
@@ -1,62 +1,51 @@
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  tags = { Name = "main-vpc" }
+# VPC
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+# Public Subnet
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
 }
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-central-1a"
-  map_public_ip_on_launch = true
+variable "public_az" {
+  description = "Availability Zone for the public subnet"
+  type        = string
+  default     = "eu-central-1a"
 }
 
-resource "aws_subnet" "private_1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "eu-central-1b"
+# Private Subnets
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for private subnet 1"
+  type        = string
+  default     = "10.0.2.0/24"
 }
 
-resource "aws_subnet" "private_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "eu-central-1c"
+variable "private_subnet_2_cidr" {
+  description = "CIDR block for private subnet 2"
+  type        = string
+  default     = "10.0.4.0/24"
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+variable "private_az_1" {
+  description = "Availability Zone for private subnet 1"
+  type        = string
+  default     = "eu-central-1b"
 }
 
-resource "aws_route" "internet" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main.id
+variable "private_az_2" {
+  description = "Availability Zone for private subnet 2"
+  type        = string
+  default     = "eu-central-1c"
 }
 
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
+# DB Subnet Group
+variable "db_subnet_group_name" {
+  description = "Name for RDS DB subnet group"
+  type        = string
+  default     = "my-db-subnet-group"
 }
-
-## modules/network/outputs.tf
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "private_subnet_ids" {
-  value = [
-    aws_subnet.private_1.id,
-    aws_subnet.private_2.id
-  ]
-}
-
-
