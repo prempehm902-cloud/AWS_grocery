a7fe3d41dbdad22b9d113852c73fd2c36e5ff456
Michael Owusu Prempeh <prempehm902@gmail.com>
2026-02-15 06:11:33 +0100

    Update variables.tf

--- infrastructure/modules/variables.tf
+++ infrastructure/modules/variables.tf
@@ -1,1 +1,30 @@
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-central-1"
+}
 
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "db_name" {
+  description = "Name of the PostgreSQL database"
+  type        = string
+  default     = "grocerymate_db"
+}
+
+variable "db_username" {
+  description = "Master username for the RDS database"
+  type        = string
+  default     = "postgres"
+}
+
+
+variable "db_password" {
+  description = "Master password for the RDS database"
+  type        = string
+  sensitive   = true
+}
